86a7e4343d94bc8f4344c9f254d610c15e59e9cb
Daisuke Maki <lestrrat+github@gmail.com>
2016-04-14 13:14:33 +0900

    Tweak test

--- events_test.go
+++ events_test.go
@@ -4,10 +4,14 @@ import "testing"
 
 func TestEventString(t *testing.T) {
 	for i := AccountsChangedEvent; i < MaxEvent; i = i << 1 {
-		
-t.Logf("%d", i)
-		if MaskToEventName(i) == "Invalid" {
+		n := MaskToEventName(i)
+		if n == "Invalid" {
 			t.Errorf("Got invalid string for %d", i)
 		}
+		t.Logf("%s = %d", n, i)
+	}
+
+	if n := EventNameToMask("MessageEvent"); n != MessageEvent {
+		t.Errorf("MessageEvent string should yield the correct mask, got %d", n)
 	}
 }
